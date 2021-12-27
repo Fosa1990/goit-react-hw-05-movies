8fb897dd992d6dcc8c6ec811493b8731431cfe3d
Fosa1990 <fosa1990@gmail.com>
2021-12-27 06:14:14 +0200

    fix Spinner styles

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
+import './Spinner.css';
 export default class App extends React.Component {
-  //other logic
   render() {
     return (
-      <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
+      <div className="loader-box">
+        <Loader
+          type="Oval"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={12000}
+        />
+      </div>
     );
   }
 }
